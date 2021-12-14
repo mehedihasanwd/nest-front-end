fe1ca7f90cd80ee85f69bfc7f14b5832c505c348
Abdur Rahman <a.rahman4124@gmail.com>
2021-12-14 12:26:14 +0600

    edit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 
 import './App.css';
+import HomePage from './Pages/Home';
 
 function App() {
-  return <h1 className="text-3xl font-bold text-red-400">Hello world!</h1>;
+  return (
+  <>
+  <h1 className="text-3xl font-bold text-red-400">Hello world!</h1>
+  <HomePage/>
+  </>
+  );
 }
 
 export default App;
